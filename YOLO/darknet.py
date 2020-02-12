8bc1f52464e984fb00acc3121a91ca485268cbb2
cusps <bunjake@gmail.com>
2020-02-12 02:01:16 -0600

    initial branch commit

---
+++ YOLO/darknet.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+
+def parse_cfg(cfgfile):
+    """
+    Takes a configuration file
+
+    Returns a list of blocks. Each blocks describes a block in the neural
+    network to be built. Block is represented as a dictionary in the list
+
+    """
